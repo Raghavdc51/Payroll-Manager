46c0d59ab3236a9b09bc0b30b49a5d8c84b18ec5
Raghavendra DC <Raghavendradc98@gmail.com>
2026-04-16 09:50:43 +0530

    Clean up myapp/apps.py by removing comments
    Removed commented-out code for AppConfig class.

--- myapp/apps.py
+++ myapp/apps.py
@@ -1,11 +1,3 @@
-# from django.apps import AppConfig
-
-
-# class MyappConfig(AppConfig):
-#     default_auto_field = 'django.db.models.BigAutoField'
-#     name = 'myapp'
-
-
 # ✅ File: myapp/apps.py
 
 from django.apps import AppConfig
