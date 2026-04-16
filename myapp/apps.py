c210556fbf9a3ae317ec911590a50583813ef3ec
Raghavdc51 <Raghavendradc98@gmail.com>
2026-04-16 09:34:24 +0530

    Update myapp/apps.py

--- myapp/apps.py
+++ myapp/apps.py
@@ -10,9 +10,9 @@
 
 from django.apps import AppConfig
 
-class MyappConfig(AppConfig):  # 👈 The class name must match your app's folder
+class MyappConfig(AppConfig):  
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'myapp'  # 👈 Make sure this matches your folder name
+    name = 'myapp'  
 
     def ready(self):
-        import myapp.signals  # 👈 This line activates your signal at app startup
+        import myapp.signals  
